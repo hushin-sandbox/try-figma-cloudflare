ac163a72b196bc19f332f9a6d508eef0549d65de
hushin <ngtv.hsn@gmail.com>
2024-01-11 20:16:49 +0900

    画像アップロード ダウンロード処理

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Hono } from 'hono';
+import { cache } from 'hono/cache';
 
 type Bindings = {
   FIGMA_TOKEN: string;
+  MY_BUCKET: R2Bucket;
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
@@ -29,12 +31,43 @@ app.post('/upload', async (c) => {
     }
   ).then((res) => res.json())) as { images: Record<string, string> };
 
-  const image = Object.values(imageResult.images)[0];
-  if (!image) {
+  const imageUrl = Object.values(imageResult.images)[0];
+  if (!imageUrl) {
     return c.json({ message: 'Invalid URL' });
   }
+  const fileName = imageUrl.split('/').at(-1) + '.png';
+  const image = await fetch(imageUrl).then((res) => res.arrayBuffer());
 
-  return c.json({ url: image });
+  await c.env.MY_BUCKET.put(fileName, image, {
+    httpMetadata: {
+      contentType: 'image/png',
+    },
+  });
+
+  return c.text(fileName);
+});
+
+app.get(
+  '*',
+  cache({
+    cacheName: 'r2-image-worker',
+  })
+);
+
+const maxAge = 60 * 60 * 24 * 30;
+
+app.get('/:key', async (c) => {
+  const key = c.req.param('key');
+
+  const object = await c.env.MY_BUCKET.get(key);
+  if (!object) return c.notFound();
+  const data = await object.arrayBuffer();
+  const contentType = object.httpMetadata?.contentType ?? '';
+
+  return c.body(data, 200, {
+    'Cache-Control': `public, max-age=${maxAge}`,
+    'Content-Type': contentType,
+  });
 });
 
 export default app;
